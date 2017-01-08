15d0e23b2b52e1c362582fef5ebc3c19c76aea94
DarkChowder <DarkChowder@hotmail.com>
2017-01-08 08:29:43 -0800

    Faster compare bytes

--- Lidgren.Network/NetUtility.cs
+++ Lidgren.Network/NetUtility.cs
@@ -319,14 +319,37 @@ namespace Lidgren.Network
 			return value;
 		}
 
-		internal static bool CompareElements(byte[] one, byte[] two)
+		internal static unsafe bool CompareElements(byte[] one, byte[] two)
 		{
 			if (one.Length != two.Length)
 				return false;
-			for (int i = 0; i < one.Length; i++)
-				if (one[i] != two[i])
-					return false;
-			return true;
+			fixed (byte* bytes1 = one, bytes2 = two)
+			{
+				int len = one.Length - 1;
+				int rem = len % 1024;
+				ulong* b1 = (ulong*)bytes1;
+				ulong* b2 = (ulong*)bytes2;
+				ulong* e1 = (ulong*)(bytes1 + len + 1 - rem);
+				while (b1 < e1)
+				{
+					if (*(b1) != *(b2) || *(b1 + 1) != *(b2 + 1) ||
+						*(b1 + 2) != *(b2 + 2) || *(b1 + 3) != *(b2 + 3) ||
+						*(b1 + 4) != *(b2 + 4) || *(b1 + 5) != *(b2 + 5) ||
+						*(b1 + 6) != *(b2 + 6) || *(b1 + 7) != *(b2 + 7) ||
+						*(b1 + 8) != *(b2 + 8) || *(b1 + 9) != *(b2 + 9) ||
+						*(b1 + 10) != *(b2 + 10) || *(b1 + 11) != *(b2 + 11) ||
+						*(b1 + 12) != *(b2 + 12) || *(b1 + 13) != *(b2 + 13) ||
+						*(b1 + 14) != *(b2 + 14) || *(b1 + 15) != *(b2 + 15))
+						return false;
+					b1 += 16;
+					b2 += 16;
+				}
+				for (int i = 0; i < rem; i++)
+				{
+					if (one[len - i] != two[len - i]) return false;
+				}
+				return true;
+			}
 		}
 
 		/// <summary>
